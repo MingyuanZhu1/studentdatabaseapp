b7588a257c92b95792f3448dabe6df8387c39bec
Mingyuan Zhu <mzhu11@student.ubc.ca>
2020-09-28 17:28:41 -0700

    added add multiple students at once option.

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student a = new Student();
-        a.enroll();
-        a.payTuition();
-        System.out.println(a);
-        // Ask how many new users we want to add
+
+
+        // Ask how many new students we want to add
+        System.out.println("Enter number of students to enroll");
+        Scanner in = new Scanner(System.in);
+        int numOfStudnets = in.nextInt();
+        Student[] students = new Student[numOfStudnets];
+        
 
         // Create n number of new students
+        for (int i = 0; i < students.length ; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println(students[i]);
+        }
     }
 }
